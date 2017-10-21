814b595e95e292fd9c765c5e5f71959ed37dc316
xuzekun <cs.xuzekun@gmail.com>
2017-10-21 18:24:34 +0800

    增加从蒙版生成词云的方法

--- wordCloud.py
+++ wordCloud.py
@@ -5,6 +5,8 @@ import jieba
 import jieba.analyse
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import numpy
+from PIL import Image
 
 
 class wordCloudof19th():
@@ -20,7 +22,7 @@ class wordCloudof19th():
         return ' '.join(seg_list)
 
     def analyse(self, content):
-        seg_list = jieba.analyse.extract_tags(content, 80 ,True)
+        seg_list = jieba.analyse.extract_tags(content, 180 ,True)
         for i in seg_list:
             print i[0], i[1]
         dic = {i[0] : i[1] for i in seg_list}
@@ -38,6 +40,12 @@ class wordCloudof19th():
         plt.axis('off')
         plt.show()
 
+    def generateByMask(self, mask, text):
+        wordCloud = WordCloud(font_path='res/simsun.ttf',mask=mask,background_color='white', max_words=180).generate_from_frequencies(text)
+        plt.imshow(wordCloud, interpolation='bilinear')
+        plt.axis('off')
+        plt.show()
+
     def wordCloudByText(self):
         content = self.getContent()
         text = self.cut(content)
@@ -48,10 +56,21 @@ class wordCloudof19th():
         text = self.analyse(content)
         self.generateByFrequency(text)
 
+    def wordCloudByMask(self):
+        content = self.getContent()
+        text = self.analyse(content)
+
+        img = Image.open('res/mask.png')
+        mask = numpy.array(img)
+        self.generateByMask(mask, text)
+
+
+
 
 
 
 if __name__ == '__main__':
     wc = wordCloudof19th()
     #wc.wordCloudByText()
-    wc.wordCloudByFrequency()
+    #wc.wordCloudByFrequency()
+    wc.wordCloudByMask()
